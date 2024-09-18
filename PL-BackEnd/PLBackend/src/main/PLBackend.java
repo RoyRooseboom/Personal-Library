cfaafa1c6baf649edc93bb5423d35de422beaf53
RoyRooseboom <brrooseboom@gmail.com>
2024-09-18 11:53:04 +0200

    Changed backend to Spring project

--- PL-BackEnd/PLBackend/src/main/PLBackend.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class PLBackend
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Our backend works :D");
-    }
-}
---
+++ PL-BackEnd/personal_library/src/main/java/project/personal/personal_library/PersonalLibraryApplication.java
@@ -0,0 +1,13 @@
+package project.personal.personal_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalLibraryApplication.class, args);
+	}
+
+}
