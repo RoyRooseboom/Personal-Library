4a738ea5bc11b2bbf7ca6c4c1d26c65424dbe0cc
RoyRooseboom <brrooseboom@gmail.com>
2024-10-02 11:41:23 +0200

    Made use of LocalStorage, start of using data in information area

--- PL-FrontEnd/PLButtons.js
+++ PL-FrontEnd/PLButtons.js
@@ -10,3 +10,13 @@ function goToMyBooks()
     document.querySelector(".interacting-window").classList.remove("disable");
     booksRequest();
 }
+
+function showInformation()
+{
+    document.querySelector(".information-area").innerHTML = localStorage.getItem("description");
+    document.querySelector(".information-area").innerHTML += localStorage.getItem("genre");
+    document.querySelector(".information-area").innerHTML += localStorage.getItem("date");
+    document.querySelector(".information-area").innerHTML += localStorage.getItem("langauge");
+    document.querySelector(".information-area").innerHTML += localStorage.getItem("wishlist");
+    document.querySelector(".information-area").innerHTML += localStorage.getItem("isbn");
+}
--- PL-FrontEnd/PLRequests.js
+++ PL-FrontEnd/PLRequests.js
@@ -6,7 +6,13 @@ function booksRequest()
         var parsedResponseText = JSON.parse(this.responseText);
         HTMLString = "";
         for(var i = 0; i < parsedResponseText.length; i++) {
-            HTMLString += "<tr><td><input type='checkbox'></td><td><button class='table-button'>" + parsedResponseText[i].title + "</button></td><td>" + parsedResponseText[i].author + "</td></tr>";
+            HTMLString += "<tr><td><input type='checkbox'></td><td><button onclick='showInformation()' class='table-button'>" + parsedResponseText[i].title + "</button></td><td>" + parsedResponseText[i].author + "</td></tr>";
+            localStorage.setItem("description", parsedResponseText[i].description);
+            localStorage.setItem("genre", parsedResponseText[i].genre);
+            localStorage.setItem("date", parsedResponseText[i].date);
+            localStorage.setItem("langauge", parsedResponseText[i].langauge);
+            localStorage.setItem("wishlist", parsedResponseText[i].wishlist);
+            localStorage.setItem("isbn", parsedResponseText[i].isbn);
         }
         
         document.getElementById("booksTable").innerHTML = HTMLString;
